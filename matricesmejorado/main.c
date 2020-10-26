5aefde667606896fdd847b654ead737ef21eaac1
NerfeMats <yermo60@hotmail.com>
2020-10-26 08:32:44 -0600

    programa que trabaja con funciones y matrices

--- matricesmejorado/main.c
+++ matricesmejorado/main.c
@@ -23,97 +23,51 @@ for(j=0;j<FILAS;j++)
 
 }
 
-void leerMatriz(int matriz[][COLUMNAS])
-{
+void leerMatriz(int m[][COLUMNAS])   // persona que espera alguien quien es ese alguien: matriz bidimensional
+{                                          // Para que espera a alguien, para hacer algun proceso
 int fil, col;
 for(fil=0;fil<FILAS;fil++)
     for(col=0; col< COLUMNAS ; col++)
     {
         printf("matriz[%d][%d]= ", fil, col);
-    scanf("%d",&matriz[fil][col]);
+    scanf("%d",&m[fil][col]);
     }
 
 
 }
 
+void sumarMatrices(int m[][COLUMNAS], int karen[][COLUMNAS], int resultado[][COLUMNAS])
+{
+int f, X;
+
+
+for(f=0;f<FILAS;f++)
+for(X=0;X<COLUMNAS;X++)
+resultado[f][X] =m[f][X]+karen[f][X];
+
+//imprimir(matrizR);
+}
 
 int main()
 {
-    /*int matriz[FILAS][COLUMNAS]={
 
-    //c0   c1   c2
-    {500, 20, -6}, // fila 0
-    {-1, 1000, 40},// fila 1
-    {30, 70, 100}   // fila 2
-    };*/
-
-
-    int matriz[FILAS][COLUMNAS], f, X;
+    int matriz[FILAS][COLUMNAS];
     int matriz1[FILAS][COLUMNAS];
     int matrizR[FILAS][COLUMNAS];
 
-leerMatriz(matriz);
+
+leerMatriz(matriz);   //funcion ya te mando a alguien: que cumpla las caracteristicas solicitadas
 leerMatriz(matriz1);
-    for(f=0;f<FILAS;f++)
-    for(X=0;X<COLUMNAS;X++)
-    matrizR[f][X] =matriz1[f][X]+matriz[f][X];
+
+
+sumarMatrices(matriz, matriz1, matrizR);
+
+
 imprimir(matriz);
 imprimir(matriz1);
 printf("La suma de la matriz A y B es:\n");
 imprimir(matrizR);
 
 
-    /*for(fil=0;fil<FILAS;fil++)
-    for(col=0; col< COLUMNAS ; col++)
-    {
-        printf("matriz[%d][%d]= ", fil, col);
-    scanf("%d",&matriz[fil][col]);
-    }*/
-
-   /* for(fil=0;fil<FILAS;fil++)
-    for(col=0; col< COLUMNAS ; col++)
-    {
-        printf("matriz1[%d][%d]= ", fil, col);
-    scanf("%d",&matriz1[fil][col]);
-    }*/
-
-
-
-
-
-
-
-/* este codigo de modelara con una funcion
-for(j=0;j<FILAS;j++)
-
-    {
-
-        for(i=0;i<COLUMNAS;i++)
-        printf("%d ", matriz[j][i]);
-
-
-        printf("\n");
-
-    }
- aqui termina el for
-
-
-
-  for(j=0;j<FILAS;j++)
-
-    {
-
-        for(i=0;i<COLUMNAS;i++)
-        printf("%d ", matrizR[j][i]);
-
-
-        printf("\n");
-
-    }
-*/
-
-print("%d", i )
-
-
-    return 0;
+return 0;
 }
